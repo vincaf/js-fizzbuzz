435ca96f5957406a63d896efe67a6ece191b5704
vincaf <vincenzocafassog@gmail.com>
2022-06-16 17:43:36 +0200

    writing the numbers on the page

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Creazione del ciclo per stampare in console numeri da 1 a 100
 for (let i = 1; i < 101; i++) {
+
+    // Inizializzazione della variabile per il box contente i numeri. Nuovo elemento div vuoto.
+    const numberBox = document.createElement('div')
+
+    // Al div vuoto si aggiungono due classi: col-2 e box
+    numberBox.classList.add('col-2', 'box', 'align-middle');
+
     // Condizioni per il valore da stampare in console
     if ((i % 3) && (i % 5) == 0) {
         console.log("FizzBuzz");
@@ -10,4 +17,10 @@ for (let i = 1; i < 101; i++) {
     } else{
         console.log(i);
     }
+
+    // All'interno del div viene inserito "i"
+    numberBox.innerHTML = i;
+
+    // Scrittura in pagina
+    document.getElementById("wrapperBox").appendChild(numberBox);
 }
